56384a63cc91e68370859fcc7a60fd24d9d1c640
RajM <marpaka@gmail.com>
2021-10-29 02:19:50 -0500

    Update udac_example_dag.py

--- udac_example_dag.py
+++ udac_example_dag.py
@@ -10,13 +10,19 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2021, 10, 25),
+    'start_date': datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catch': False,
+    'email_on_retry': False
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          schedule_interval='0 * * * *',
+          max_active_runs=1
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
@@ -94,7 +100,7 @@ run_quality_checks = DataQualityOperator(
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-#start_operator >> stage_songs_to_redshift
+start_operator >> stage_songs_to_redshift
 #start_operator >> stage_events_to_redshift
 
 #(stage_events_to_redshift,stage_songs_to_redshift) >> load_songplays_table
